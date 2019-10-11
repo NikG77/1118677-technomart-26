0cb971b4c0cc8355147c590a2a081f89ecd9be09
Georg <georgiy.nikitin@gmail.com>
2019-10-12 01:12:40 +0300

    Мелкие исправления

---
+++ js/buy.js
@@ -0,0 +1,24 @@
+var buyLink = document.querySelector(".item-buy");
+
+var buyPopup = document.querySelector(".modal-buy");
+var buyClose = buyPopup.querySelector(".modal-close");
+
+buyLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyPopup.classList.add("modal-show-buy");
+});
+
+buyClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyPopup.classList.remove("modal-show-buy");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (buyPopup.classList.contains("modal-show-buy")) {
+      buyPopup.classList.remove("modal-show-buy");
+    }
+  }
+});
+
